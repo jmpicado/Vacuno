cd8816dd7f6093d9c6a63b65171640c2843b5610
Jose Maria Picado <picadoloiro@gmail.com>
2015-10-16 09:50:02 +0200

    First Version

---
+++ MyApplication2/app/src/main/java/com/example/vacuno1/app/VacuumActivity.java
@@ -0,0 +1,69 @@
+package com.example.vacuno1.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+public class VacuumActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacuum);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void back(View button) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void save(View button) {
+        // Do click handling here
+        final EditText nameField = (EditText) findViewById(R.id.EditTextName);
+        String name = nameField.getText().toString();
+
+        final EditText emailField = (EditText) findViewById(R.id.EditTextEmail);
+        String email = emailField.getText().toString();
+
+        final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);
+        String feedback = feedbackField.getText().toString();
+
+        final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
+        String feedbackType = feedbackSpinner.getSelectedItem().toString();
+
+        final CheckBox responseCheckbox = (CheckBox) findViewById(R.id.CheckBoxResponse);
+        boolean bRequiresResponse = responseCheckbox.isChecked();
+    }
+
+
+
+}
